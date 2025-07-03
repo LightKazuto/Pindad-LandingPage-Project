fae160414c294eff57a35e794c1f1d4559a7151c
LightKazuto <ilhamsidi62@gmail.com>
2025-07-03 09:06:54 +0700

    update: add routing for 404

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './index.css';
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from './Pages/LandingPages';
+import Page404 from './Pages/404';
 
 function App() {
   return (
-      <Routes>
-        <Route path='/' element={<LandingPage />} />
-      </Routes>
+    <Routes>
+      <Route path='/' element={<LandingPage />} />
+      <Route path='*' element={<Page404 />} />
+    </Routes>
   );
 }
 
