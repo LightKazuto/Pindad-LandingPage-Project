a6662f67f80ab69163b5d667e15e6d8f57a7e9f5
LightKazuto <ilhamsidi62@gmail.com>
2025-07-03 10:12:23 +0700

    update: add routing for 403 and 500

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './index.css';
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from './Pages/LandingPages';
-import Page404 from './Pages/404';
+import Page404 from './Badrequest/404';
+import Page500 from './Badrequest/500';
+import Page403 from './Badrequest/403';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<LandingPage />} />
+
+      <Route path='/500' element={<Page500 />} />
+      <Route path='/403' element={<Page403 />} />
+
       <Route path='*' element={<Page404 />} />
     </Routes>
   );
